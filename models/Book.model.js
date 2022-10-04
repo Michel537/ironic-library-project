4507a579441d765c8f7474906bc265705f427d4f
Michel Pomares <mapomarest@gmail.com>
2022-10-04 11:53:40 +0200

    update seeds file- added Authors Model

--- models/Book.model.js
+++ models/Book.model.js
@@ -4,8 +4,11 @@ const bookSchema = new Schema(
     {
         title: String,
         description: String,
-        author: String,
-        rating: Number
+        rating: Number,
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: "Author"
+        },
     },
     {
         timestamps: true
@@ -13,3 +16,4 @@ const bookSchema = new Schema(
 );
 
 module.exports = model('Book', bookSchema);
+
